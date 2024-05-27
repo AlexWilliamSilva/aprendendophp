8be83f7701724f2a792d14ebf297d6798ed4e804
alex <alexwilliamgomessilva168@gmail.com>
2024-05-27 13:45:50 -0300

    final login

---
+++ crud/script.sql
@@ -0,0 +1,14 @@
+/*CREATE TABLE produto
+(Id INT PRIMARY KEY AUTO_INCREMENT,
+ Nome VARCHAR(60) NOT NULL,
+ Descricao VARCHAR(255) NOT NULL,
+ Unid VARCHAR(3) NOT NULL,
+ Marca VARCHAR(60) NOT NULL
+)*/
+ 
+/*CREATE TABLE usuario (
+    Id int not null primary key auto_increment,
+    Nome varchar(100),
+    Usuario varchar (100),
+    Senha varchar(100)
+)*/
