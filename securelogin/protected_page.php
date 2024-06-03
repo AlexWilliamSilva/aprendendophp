a557823c02595646979d13119f4c920f95a03253
alex <alexwilliamgomessilva168@gmail.com>
2024-06-03 15:20:22 -0300

    commit save 03/06

--- securelogin/protected_page.php
+++ securelogin/protected_page.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Secure Login: Protected Page</title>
-    <link rel="stylesheet" href="styles/main.css" />
+    <link rel="stylesheet" href="styles/main.css" /> 
 </head>
 <body>
     <?php if(login_check($mysqli) == true):?> 
