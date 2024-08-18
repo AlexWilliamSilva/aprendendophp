5efa6b5d226163f969d881e5c12ff6efd42af07b
Alexoficial01 <00001081377847sp@al.educacao.sp.gov.br>
2024-08-18 11:44:44 -0300

    arrumando erros da pasta "CRUD"

--- crud/script.sql
+++ crud/script.sql
@@ -1,22 +1,25 @@
-/*Create table cliente (
+create database tutocrudphp;
+
+Create table tutocrudphp.cliente (
     Id int primary key auto_increment,
     Nome varchar(60) not null,
     Email varchar(150) not null, 
     Cidade varchar(100),
     UF varchar(2)
-)*/
+)
 
-/*CREATE TABLE produto
+CREATE TABLE tutocrudphp.produto
 (Id INT PRIMARY KEY AUTO_INCREMENT,
  Nome VARCHAR(60) NOT NULL,
  Descricao VARCHAR(255) NOT NULL,
  Unid VARCHAR(3) NOT NULL,
  Marca VARCHAR(60) NOT NULL
-)*/
+)
  
-/*CREATE TABLE usuario (
+
+CREATE TABLE tutocrudphp.usuario (
     Id int not null primary key auto_increment,
     Nome varchar(100),
     Usuario varchar (100),
     Senha varchar(100)
-)*/
+)
